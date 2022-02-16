726a86e8ce3964810adce427362d43bfb6e0177e
prireds174 <pri.reds174@gmail.com>
2022-02-16 12:10:48 -0500

    90%

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -10,25 +10,25 @@ function Navigation(props) {
         <div className="container-fluid">
       <Link to="/home" className="navbar-brand">
       
-      <a href="https://fontmeme.com/bobs-burgers-tv-show-font/"><img src="https://fontmeme.com/permalink/220216/35ae46a0faf484314bf9ce88a61f3374.png" alt="bobs-burgers-tv-show-font" border="0" style={{
+      <img src="https://fontmeme.com/permalink/220216/35ae46a0faf484314bf9ce88a61f3374.png" alt="bobs-burgers-tv-show-font" border="0" style={{
         paddingLeft:"25px",
-        paddingBottom: "65px"}}/></a>
+        paddingBottom: "65px"}}/>
     </Link>
     
   </div>
 </nav>
 
 
-<ul className="nav bg-dark bg-gradient justify-content-center">
+<ul className="nav bg-dark bg-gradient p-2 bg-opacity-85  justify-content-center">
 <li className="nav-item">
-      <Link to="/home"className="nav-link"><img src="https://a.thumbs.redditmedia.com/f5GxaATVn5u-M6Nz5r6pNHcf8bpW7bLhjheBKv-BnS0.png" alt="" width="25" height="15" style={{
-        paddingRight:"15px"}} className="d-inline-block text-center"/>Home
+      <Link to="/home"className="nav-link"><img src="https://img.icons8.com/external-icongeek26-flat-icongeek26/64/000000/external-burger-baking-and-bakery-icongeek26-flat-icongeek26.png" alt="" width="45" height="35" style={{
+        paddingRight:"15px"}} className="d-inline-block text-center"/>
       </Link>
     
   </li>
 
   <li className="nav-item">
-      <Link to="/characters" className="nav-link active text" aria-current="page">
+      <Link to="/characters" className="nav-link" aria-current="page">
           Characters
           </Link>
     
